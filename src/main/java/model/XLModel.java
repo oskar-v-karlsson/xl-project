f58ac51507e3678b8e7f0248162439aa7e0a498d
Zairian <oskar.karlsson123@outlook.com>
2021-05-18 16:05:29 +0200

    Fixed circular reference error

--- src/main/java/model/XLModel.java
+++ src/main/java/model/XLModel.java
@@ -15,6 +15,7 @@ public class XLModel implements Environment {
   public static final int COLUMNS = 10, ROWS = 10;
   private Map<String, CellContent> map;
   private List<ModelObserver> list;
+  private Set<CellContent> visited = new HashSet<>();
 
   public XLModel(){
 
@@ -29,16 +30,41 @@ public class XLModel implements Environment {
    * @param text    the new code for the cell - can be raw text (starting with #) or an expression
    */
   public void update(CellAddress address, String text) {
+    visited.clear();
+
     map.put(address.toString(), CellCreator.createContentType(text));
-
-    for(int i=0; i < list.size(); i++){
-      list.get(i).modelChange(address.toString(), map.get(address.toString()).getContent(this));
+    if(checkCircular(address, text)){
+      for(int i=0; i < list.size(); i++){
+        list.get(i).modelChange(address.toString(), new ErrorResult("Circular Reference").toString());
+      }
+    } else{
+      for(int i=0; i < list.size(); i++){
+        list.get(i).modelChange(address.toString(), map.get(address.toString()).getContent(this));
+      }
+      updateAll();
     }
-
-    updateAll();
   }
 
+  private boolean checkCircular(CellAddress address, String text){
+    if(text.contains(address.toString())){
+      return true;
+    } else {
+      String[] split = text.split("\\+|-|\\*|/");
+      for (String s: split
+           ) {
+        if(map.get(s) == null){
+          return false;
+        }
+        return checkCircular(address, map.get(s).getEditorText());
+      }
+    }
+    return false;
+  }
+
+
+
   private void updateAll(){
+    visited.clear();
     for (Map.Entry<String, CellContent> e: map.entrySet()
          ) {
       for(int i=0; i < list.size(); i++){
@@ -102,4 +128,5 @@ public class XLModel implements Environment {
       return new ErrorResult("Referencing empty cell");
     }
   }
+
 }
